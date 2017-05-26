9510cbf60e2d202ca8f7a1078bdae1862c7aed9f
Marypd97 <mariangela.pacheco@ucsp.edu.pe>
2017-05-25 23:54:54 -0500

    created IPlayListRepository

--- ProyectoFinal/src/main/java/com/espringjpa/repository/IPlaylistRepository.java
+++ ProyectoFinal/src/main/java/com/espringjpa/repository/IPlaylistRepository.java
@@ -1,5 +1,17 @@
 package com.espringjpa.repository;
 
-public class IPlaylistRepository {
+import java.util.Collection;
 
+import com.espringjpa.domain.PlayList;
+
+public interface IPlaylistRepository {
+
+	void save(PlayList PlayList);
+
+	PlayList get(String name); // name? o id?
+
+	Collection<PlayList> getAll();
+
+	void remove(String name);// name o id?
+	
 }
