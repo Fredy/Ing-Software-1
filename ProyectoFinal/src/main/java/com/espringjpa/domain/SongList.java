40b87d51ca94e416a4ec4ad34c6217fe70bfbbfe
Marypd97 <mariangela.pacheco@ucsp.edu.pe>
2017-05-27 02:00:18 -0500

    Mapping with Song

--- ProyectoFinal/src/main/java/com/espringjpa/domain/SongList.java
+++ ProyectoFinal/src/main/java/com/espringjpa/domain/SongList.java
@@ -1,7 +1,21 @@
 package com.espringjpa.domain;
 import java.util.Vector;
 
+import javax.persistence.*;
+
+
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
 public abstract class SongList {
+	
+	@Id
+	@Column(name = "id")
+	private Long idSongList;
+	
+	@OneToMany
+	@JoinTable(name = "songList_song", 
+		joinColumns = @JoinColumn(name = "idSongList", referencedColumnName = "id"),
+		inverseJoinColumns = @JoinColumn(name = "idSong", referencedColumnName = "id"))
 	protected Vector<Song> songList;
 
 	public void addSong (Song newSong){
