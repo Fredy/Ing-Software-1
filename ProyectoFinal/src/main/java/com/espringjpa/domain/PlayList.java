dc4500aaef6a07eaea2a6c4dcca474fa65e002a6
Marypd97 <mariangela.pacheco@ucsp.edu.pe>
2017-05-27 13:14:15 -0500

    add id

--- ProyectoFinal/src/main/java/com/espringjpa/domain/PlayList.java
+++ ProyectoFinal/src/main/java/com/espringjpa/domain/PlayList.java
@@ -3,18 +3,32 @@ package com.espringjpa.domain;
 //import java.util.Vector;
 
 import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import java.util.List;
+import java.util.Vector;
+
+import javax.persistence.*;
+
 
 @Entity 
 public class PlayList extends SongList {
 
+	@Id
+	protected long id;
 	protected String name;
 	protected Date creationDate;
 	
+//	@OneToMany
+//		@JoinTable(name = "songList_song", 
+//		joinColumns = @JoinColumn(name = "idSongList", referencedColumnName = "id"),
+//		inverseJoinColumns = @JoinColumn(name = "idSong", referencedColumnName = "id"))
+	protected Vector<Song> songList;
+	
+//	@ManyToMany
+//    @JoinTable(
+//            name = "followedPlayListUser",
+//            joinColumns = @JoinColumn(name = "PlayListID", referencedColumnName = "id"),
+//            inverseJoinColumns = @JoinColumn(name="UserID", referencedColumnName = "id"))
+    private List<User> followers;
 	
 	public String getName(){
 		return name;
