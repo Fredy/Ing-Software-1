7f9af3f303c2aca63b956ec157c75aadc092587c
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-05-27 13:26:35 -0500

    -User, Admin interfaces: changes type of id

--- ProyectoFinal/src/main/java/com/espringjpa/repository/IUserRepository.java
+++ ProyectoFinal/src/main/java/com/espringjpa/repository/IUserRepository.java
@@ -4,7 +4,7 @@ import com.espringjpa.domain.User;
 
 public interface IUserRepository {
     void save(User user);
-    User get(String idNumber);
+    User get(long idNumber);
     Collection<User> getAll();
     void remove(String idNumber);
     // TODO: add id's and db connection in User class
