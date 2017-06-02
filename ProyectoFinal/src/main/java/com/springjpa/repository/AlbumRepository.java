06e1a0baf52293010a0a1dab22611505711edfd7
Alfred <alfredchavezurday@gmail.com>
2017-06-02 22:43:43 +0000

    AlbumRepository and ArtistRepository find implemented

--- ProyectoFinal/src/main/java/com/springjpa/repository/AlbumRepository.java
+++ ProyectoFinal/src/main/java/com/springjpa/repository/AlbumRepository.java
@@ -4,8 +4,16 @@ package com.springjpa.repository;
  * Created by MathHellscream
  */
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.springjpa.domain.Album;
 
+import java.util.Collection;
+
 public interface AlbumRepository extends CrudRepository<Album,Long>{
+    @Query("select a from Album a where a.id = ?1")
+    Album findOne(Long id);
+
+    @Query("select a from Album a")
+    Collection<Album> findAll();
 }
--- ProyectoFinal/src/main/java/com/springjpa/repository/ArtistRepository.java
+++ ProyectoFinal/src/main/java/com/springjpa/repository/ArtistRepository.java
@@ -4,8 +4,16 @@ package com.springjpa.repository;
  * Created by MathHellscream
  */
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.springjpa.domain.Artist;
 
+import java.util.Collection;
+
 public interface ArtistRepository extends CrudRepository<Artist,Long> {
+    @Query("SELECT a from Artist a where a.id=?1")
+    Artist findOne(Long id);
+
+    @Query("SELECT a from Artist a")
+    Collection<Artist> findAll();
 }
