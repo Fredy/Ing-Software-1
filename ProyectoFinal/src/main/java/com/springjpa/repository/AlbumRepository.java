d2a25f9a1b30f2865ba7bcd4656541202b03c208
Alfred <alfredchavezurday@gmail.com>
2017-06-15 17:48:12 +0000

    AlbumRepository and ArtistRepository find implemented

--- ProyectoFinal/src/main/java/com/springjpa/repository/AlbumRepository.java
+++ ProyectoFinal/src/main/java/com/springjpa/repository/AlbumRepository.java
@@ -12,8 +12,15 @@ import com.springjpa.domain.Album;
 import java.util.Collection;
 
 public interface AlbumRepository extends CrudRepository<Album,Long>{
+
     @Query("select a from Album a where a.id = ?1")
     Album findOne(Long id);
+
+    @Query("select a.id from Album a where a.name = ?1")
+    Collection<Long> findByName(String name);
+
+    @Query("select a.name from Album a where a.id = ?1")
+    Collection<String> findNamebyId(Long id);
 
     @Query("select a from Album a")
     Collection<Album> findAll();
@@ -26,4 +33,10 @@ public interface AlbumRepository extends CrudRepository<Album,Long>{
 
     @Query("select b from Album a INNER join a.artists b where a.id=?1")
     Collection<Artist> getArtists(Long AlbumId);
+
+    @Query("select b.name from Album a INNER join a.artists b where a.id=?1")
+    Collection<String> getArtistsName(Long AlbumId);
+
+    @Query("select b.id from Album a INNER join a.artists b where a.id=?1")
+    Collection<Long> getArtistsId(Long AlbumId);
 }
