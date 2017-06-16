5e048332d2167715a82518144de726654122580b
JcCcari <jhoel.ccari@ucsp.edu.pe>
2017-06-16 12:24:55 -0500

    SongController modified

--- ProyectoFinal/src/main/java/com/springjpa/controller/SongController.java
+++ ProyectoFinal/src/main/java/com/springjpa/controller/SongController.java
@@ -5,6 +5,7 @@ import com.springjpa.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
@@ -19,25 +20,33 @@ public class SongController {
     @Autowired
     SongService songService;
 
-    @RequestMapping("/songs")
+    @RequestMapping(value ="/insertSong", method = RequestMethod.POST)
     @ResponseBody
-    Collection<Song> showSongs(){
+    public void addSong(Song song){
+        songService.save(song);
+    }
+
+    @RequestMapping(value = "/songs", method = RequestMethod.GET)
+    @ResponseBody
+    public Collection<Song> showSongs(){
         return songService.showSongs();
     }
 
-    @RequestMapping("top10BestRated")
-    Collection<Song> top10BestRated(){
+    @RequestMapping(value = "/top10BestRated", method = RequestMethod.GET)
+    @ResponseBody
+    public Collection<Song> top10BestRated(){
         return songService.top10BestRating();
     }
 
-    @RequestMapping("top100MostPlayed")
-    Collection<Song> top100MostPlayed(){
+    @RequestMapping(value = "/top100MostPlayed", method = RequestMethod.GET)
+    @ResponseBody
+    public Collection<Song> top100MostPlayed(){
         return songService.top100MostPlayed();
     }
 
-    @RequestMapping("findByName")
-    Song findByName(String song){
+    @RequestMapping(value = "/findByName", method = RequestMethod.GET)
+    @ResponseBody
+    public Song findByName(String song){
         return songService.findByName(song);
     }
-
 }
