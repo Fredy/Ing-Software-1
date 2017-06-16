507fc8624336619c8a9d4fd917908ab2a6cfddfd
JcCcari <jhoel.ccari@ucsp.edu.pe>
2017-06-15 23:27:43 -0500

    update SongService and SongRepository

--- ProyectoFinal/src/main/java/com/springjpa/service/SongService.java
+++ ProyectoFinal/src/main/java/com/springjpa/service/SongService.java
@@ -5,6 +5,7 @@ import com.springjpa.repository.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.CollationElementIterator;
 import java.util.Collection;
 
 /**
@@ -15,8 +16,8 @@ public class SongService {
     @Autowired
     SongRepository songRepository;
 
-    public Boolean findSong(String nameSong){
-        if (songRepository.findByName(nameSong) != null)
+    public Boolean findSong(String songName){
+        if (songRepository.findByName(songName) != null)
             return Boolean.TRUE;
         return Boolean.FALSE;
     }
@@ -29,4 +30,17 @@ public class SongService {
         return songRepository.findAll();
     }
 
+    public Song findByName(String songName){
+        return songRepository.findByName(songName);
+    }
+
+    public Collection<Song> top10BestRating(){
+        Collection<Song> result = songRepository.top10BestRating();//.setMaxResults(10).getResultList;
+        return result;
+    }
+
+    public Collection<Song> top100MostPlayed() {
+        Collection<Song> result = songRepository.top100MostPlayed();//.setMaxResults(100).getResultList;
+        return result;
+    }
 }
