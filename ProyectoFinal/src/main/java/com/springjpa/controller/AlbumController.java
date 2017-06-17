a7362f0612f78765c1fb7f3a7d83840b3060a507
Alfred <alfredchavezurday@gmail.com>
2017-06-17 14:26:19 +0000

    Added Artist from album

--- ProyectoFinal/src/main/java/com/springjpa/controller/AlbumController.java
+++ ProyectoFinal/src/main/java/com/springjpa/controller/AlbumController.java
@@ -1,7 +1,26 @@
 package com.springjpa.controller;
 
+import com.springjpa.domain.Artist;
+import com.springjpa.service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+
 /**
  * Created by alfred on 6/16/17.
  */
+@Controller
 public class AlbumController {
+    @Autowired
+    private AlbumService albumService;
+
+   @RequestMapping("/getArtistsOfAlbum")
+   @ResponseBody
+    Collection<String> getArtistofAlbum(@RequestParam Long id){
+       return albumService.getArtistsNameofAlbum(id);
+   }
 }
--- ProyectoFinal/src/main/webapp/js/artist.js
+++ ProyectoFinal/src/main/webapp/js/artist.js
@@ -5,7 +5,7 @@
 
 var app = angular.module('artistName', []);
 app.controller('Ctrl', function($scope, $http) {
-    $http.get("artist").then(function(response) {
-            $scope.showArtist= response.data[0];
+    $http.get("/Artist?id=1").then(function(response) {
+            $scope.showArtist= response.data;
         });
 });
