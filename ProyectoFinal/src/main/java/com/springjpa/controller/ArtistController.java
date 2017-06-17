37f49d584fcd3c7dae77193038e2dfc938ed178d
Alfred <alfredchavezurday@gmail.com>
2017-06-17 07:30:50 +0000

    Updating2

--- ProyectoFinal/src/main/java/com/springjpa/controller/ArtistController.java
+++ ProyectoFinal/src/main/java/com/springjpa/controller/ArtistController.java
@@ -5,6 +5,7 @@ import com.springjpa.service.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,7 +18,7 @@ public class ArtistController {
     @Autowired
     private ArtistService artistService;
 
-    @RequestMapping("/artist.html")
+    @RequestMapping("/Artist")
     @ResponseBody
     String getArtistName(@RequestParam Long id){
         return artistService.findArtistNameById(id);
--- ProyectoFinal/src/main/webapp/js/artist.js
+++ ProyectoFinal/src/main/webapp/js/artist.js
@@ -5,8 +5,7 @@
 
 var app = angular.module('artistName', []);
 app.controller('Ctrl', function($scope, $http) {
-    $http.get("Artist?id=1")
-        .then(function(response) {
-            $scope.showArtist= response.data;
+    $http.get("artist").then(function(response) {
+            $scope.showArtist= response.data[0];
         });
 });
