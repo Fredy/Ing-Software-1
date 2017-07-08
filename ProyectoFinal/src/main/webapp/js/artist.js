631001ae64bbeebc6764e16535ab1cac44f58b81
Alfred <alfredchavezurday@gmail.com>
2017-07-08 07:59:10 +0000

    I can get id

--- ProyectoFinal/src/main/webapp/js/artist.js
+++ ProyectoFinal/src/main/webapp/js/artist.js
@@ -4,15 +4,29 @@
 
 
 var app = angular.module('artistName', []);
+var value;
+if((window.location.href).indexOf('?') !== -1) {
+    var queryString = (window.location.href).substr((window.location.href).indexOf('?') + 1);
 
-app.controller('Ctrl', function($scope, $http) {
-    $http.get("/ObjArtist?id=1").then(function(response) {
+    // "queryString" will now contain kerdesPost=fdasdas%20ad%20asd%20ad%20asdas
+
+    value = (queryString.split('='))[1];
+
+    // "value" will now contain fdasdas%20ad%20asd%20ad%20asdas
+
+    //value = decodeURIComponent(value);
+
+    // "value" will now contain fdasdas ad asd ad asdas (unescaped value)
+}
+
+app.controller('Ctrl',function($scope, $http) {
+    $http.get("/ObjArtist?id="+value).then(function(response) {
             $scope.showArtist = response.data;
         });
 });
 
 app.controller('AlbumCtrl', function($scope, $http) {
-    $http.get("/AlbumsOfArtist?id=1").then(function(response) {
+    $http.get("/AlbumsOfArtist?id="+value).then(function(response) {
         $scope.showAlbums = response.data;
     });
 });
